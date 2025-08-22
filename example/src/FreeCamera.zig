b912d38728b6dcd88156403bca0e665b55d199d0
HaraldWik <wikhallharald@gmail.com>
2025-08-22 20:18:45 +0200

    Updated organization, updated 'README.md'

--- example/src/FreeCamera.zig
+++ example/src/FreeCamera.zig
@@ -10,29 +10,29 @@ was_rotating: bool = false,
 pub fn update(
     self: *@This(),
     pipeline: engine.gfx.Pipeline,
-    window: engine.Window,
+    app: engine.App,
     delta_time: f32,
 ) !void {
-    if (window.isKeyDown(.p)) std.debug.print("{any}\n", .{self.transform});
+    if (app.isKeyDown(.p)) std.debug.print("{any}\n", .{self.transform});
     const pitch = &self.transform.rotation[0];
     const yaw = &self.transform.rotation[1];
 
-    const mouse = engine.c.SDL_GetMouseState(null, null);
+    const mouse = engine.sdl.SDL_GetMouseState(null, null);
 
     var relative_x: f32 = undefined;
     var relative_y: f32 = undefined;
-    _ = engine.c.SDL_GetRelativeMouseState(&relative_x, &relative_y);
+    _ = engine.sdl.SDL_GetRelativeMouseState(&relative_x, &relative_y);
 
     if (mouse == 4) {
-        if (!engine.c.SDL_HideCursor()) return error.SdlHideCursor;
+        if (!engine.sdl.SDL_HideCursor()) return error.SdlHideCursor;
 
         yaw.* += relative_x * self.sensitivity;
         pitch.* += relative_y * self.sensitivity;
         self.was_rotating = true;
     } else if (self.was_rotating) {
-        _ = engine.c.SDL_GetRelativeMouseState(null, null);
+        _ = engine.sdl.SDL_GetRelativeMouseState(null, null);
         self.was_rotating = false;
-        if (!engine.c.SDL_ShowCursor()) return error.SdlShowCursor;
+        if (!engine.sdl.SDL_ShowCursor()) return error.SdlShowCursor;
     }
 
     pitch.* = std.math.clamp(pitch.*, -89.9, 89.9);
@@ -53,25 +53,20 @@ pub fn update(
     var move = nz.Vec3(f32){ 0, 0, 0 };
     const velocity = self.speed * delta_time;
 
-    if (window.isKeyDown(.w)) move -= nz.vec.scale(forward, velocity);
-    if (window.isKeyDown(.s)) move += nz.vec.scale(forward, velocity);
-    if (window.isKeyDown(.a)) move += nz.vec.scale(right, velocity);
-    if (window.isKeyDown(.d)) move -= nz.vec.scale(right, velocity);
-    if (window.isKeyDown(.space)) move -= nz.vec.scale(up, velocity);
-    if (window.isKeyDown(.lctrl)) move += nz.vec.scale(up, velocity);
+    if (app.isKeyDown(.w)) move -= nz.vec.scale(forward, velocity);
+    if (app.isKeyDown(.s)) move += nz.vec.scale(forward, velocity);
+    if (app.isKeyDown(.a)) move += nz.vec.scale(right, velocity);
+    if (app.isKeyDown(.d)) move -= nz.vec.scale(right, velocity);
+    if (app.isKeyDown(.space)) move -= nz.vec.scale(up, velocity);
+    if (app.isKeyDown(.lctrl)) move += nz.vec.scale(up, velocity);
 
-    // if (keyboard[sdl.SDL_SCANCODE_UP])
-    //     camera.speed += 10;
-    // if (keyboard[sdl.SDL_SCANCODE_DOWN])
-    //     camera.speed -= 10;
-
-    const speed_multiplier: f32 = if (!window.isKeyDown(.lshift)) 1 else 4;
+    const speed_multiplier: f32 = if (app.isKeyDown(.lshift)) 4 else if (app.isKeyDown(.lctrl)) 0.1 else 1;
 
     self.speed = std.math.clamp(self.speed, 0, 1000);
 
     self.transform.position += nz.vec.scale(move, speed_multiplier);
 
-    if (window.isKeyDown(.r)) {
+    if (app.isKeyDown(.r)) {
         yaw.* = 0;
         pitch.* = 0;
         self.transform.position = .{ 0, 0, 0 };
@@ -82,7 +77,7 @@ pub fn update(
     view = view.mul(.rotate(std.math.degreesToRadians(yaw.*), .{ 0, 1, 0 }));
     view = view.mul(.translate(self.transform.position));
 
-    const projection: nz.Mat4x4(f32) = .perspective(std.math.degreesToRadians(45.0), try window.getAspect(), 1, 500.0);
+    const projection: nz.Mat4x4(f32) = .perspective(std.math.degreesToRadians(45.0), try app.window.getAspect(), 1, 500.0);
 
     try pipeline.setUniform("u_projection", .{ .mat4x4 = projection.d });
     try pipeline.setUniform("u_view", .{ .mat4x4 = view.d });
--- example/src/main.zig
+++ example/src/main.zig
@@ -6,13 +6,16 @@ const gl = @import("gl");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    const window: engine.Window = try .init("super-windows-world", 900, 900);
-    defer window.deinit();
+    const app: engine.App = try .init("Application", 900, 900);
+    defer app.deinit();
+
+    const ui_ctx: engine.ui.Context = try .init(app);
+    defer ui_ctx.deinit();
 
     const audio_device: engine.audio.Device = try .init();
     defer audio_device.deinit();
 
-    const gfx_context: engine.gfx.Context = try .init(window.handle);
+    const gfx_context: engine.gfx.Context = try .init(app.window.toC());
     defer gfx_context.deinit();
 
     const asset_manager: engine.AssetManager = try .init(allocator, audio_device);
@@ -30,21 +33,21 @@ pub fn main() !void {
     const terrain_model = try terrain.toModel(allocator);
 
     var time: f32 = 0;
-    while (!window.shouldClose()) {
-        const delta_time = window.getDeltaTime();
+    while (!app.shouldClose()) {
+        const delta_time = app.getDeltaTime();
         time += delta_time;
         try gfx_context.clear();
 
         // std.debug.print("FPS: {d:.2}\n", .{1 / delta_time});
 
         pipeline.bind();
-        try free_camera.update(pipeline, window, delta_time);
+        try free_camera.update(pipeline, app, delta_time);
 
         for (0..100) |i| {
             const f: f32 = @floatFromInt(i);
             var transform: nz.Transform(f32) = .{ .position = .{ std.math.round(@mod(f, 10) * 3), @sin(time + f), std.math.round(f / 10 * 3) } };
 
-            transform.rotation = @splat(@mod(transform.rotation[1] + 30 * window.getDeltaTime(), 360));
+            transform.rotation = @splat(@mod(transform.rotation[1] + 30 * app.getDeltaTime(), 360));
             asset_manager.getTexture("error_wall.jpg").bind(0);
             try pipeline.setUniform("u_model", .{ .mat4x4 = transform.toMat4x4().d });
             asset_manager.getModel("cube.obj").draw();
@@ -59,10 +62,10 @@ pub fn main() !void {
 
         try gfx_context.present();
 
-        if (window.isKeyDown(.up)) volume += 0.01;
-        if (window.isKeyDown(.down)) volume -= 0.01;
+        if (app.isKeyDown(.up)) volume += 0.01;
+        if (app.isKeyDown(.down)) volume -= 0.01;
 
-        if (window.isKeyDown(.o)) {
+        if (app.isKeyDown(.o)) {
             volume = @max(0, volume);
             try asset_manager.getSound("bell.wav").play(volume);
         }
